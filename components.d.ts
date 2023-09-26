f38a12a40408c9b0fac5bd6df7fb12d575bd3814
Liuqi <liuqi6602@163.com>
2023-09-26 17:09:58 +0800

    a

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
-    ElBadge: typeof import('element-plus/es')['ElBadge']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElCol: typeof import('element-plus/es')['ElCol']
