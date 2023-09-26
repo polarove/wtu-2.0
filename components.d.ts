b162c62ad86b80d8eb2196d22b2864bbaafffef3
Liuqi <liuqi6602@163.com>
2023-09-26 12:19:13 +0800

    修改版本号为1.0.1_alpha_release

--- components.d.ts
+++ components.d.ts
@@ -20,7 +20,6 @@ declare module 'vue' {
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElOption: typeof import('element-plus/es')['ElOption']
