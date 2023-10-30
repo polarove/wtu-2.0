c20cb82fd5704976f3d991fb5ae7908ffa6e7884
Liuqi <liuqi6602@163.com>
2023-10-30 13:16:33 +0800

    save

--- src/api/warframestat/index.ts
+++ src/api/warframestat/index.ts
@@ -1,74 +1,63 @@
-import { isBlank } from '@/util/StrUtil'
 import warframestat from './instance'
+import { authStore } from '@/store'
+const _authStore = authStore()
 
-export const getFissureList = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/fissures/`, { language: 'zh' })
+export const getFissureList = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/fissures/`, {
+        language: 'zh',
+    })
 }
 
-export const getEarthCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/earthCycle/`, { language: 'zh' })
+export const getEarthCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/earthCycle/`, {
+        language: 'zh',
+    })
 }
 
-export const getCetusCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/cetusCycle/`, { language: 'zh' })
+export const getCetusCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/cetusCycle/`, {
+        language: 'zh',
+    })
 }
 
-export const getCambionCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/cambionCycle/`, { language: 'zh' })
+export const getCambionCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/cambionCycle/`, {
+        language: 'zh',
+    })
 }
 
-export const getVallisCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/vallisCycle/`, { language: 'zh' })
+export const getVallisCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/vallisCycle/`, {
+        language: 'zh',
+    })
 }
 
-export const getZarimanCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/zarimanCycle/`, { language: 'zh' })
+export const getZarimanCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/zarimanCycle/`, {
+        language: 'zh',
+    })
 }
 
-export const getVoidTraderCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/voidTrader/`, { language: 'zh' })
+export const getVoidTraderCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/voidTrader/`, {
+        language: 'zh',
+    })
 }
 
-export const getDuviriCycle = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/duviriCycle/`, { language: 'zh' })
+export const getDuviriCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/duviriCycle/`, {
+        language: 'zh',
+    })
 }
 
-export const getArbitrationCycle = (
-    platform?: 'pc' | 'ps4' | 'xb1' | 'swi'
-) => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/arbitration/`, { language: 'zh' })
+export const getArbitrationCycle = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/arbitration/`, {
+        language: 'zh',
+    })
 }
 
-export const getAlerts = (platform?: 'pc' | 'ps4' | 'xb1' | 'swi') => {
-    if (isBlank(platform)) {
-        platform = 'pc'
-    }
-    return warframestat.get(`/${platform}/alerts/`, { language: 'zh' })
+export const getAlerts = () => {
+    return warframestat.get(`/${_authStore.getServerChar()}/alerts/`, {
+        language: 'zh',
+    })
 }
