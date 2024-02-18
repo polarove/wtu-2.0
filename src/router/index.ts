ecf30446305393ad9b862760a9b0b0265c66f321
liuqi <liuqi6602@163.com>
2024-02-18 23:43:51 +0800

    f

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import nProgress from 'nprogress'
 import entries from '@/composables/entries'
 
@@ -151,7 +151,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 })
 
