7aa593cefd290a9ee3bbd7c6c9bf21ea8dc5885b
liuqi <liuqi6602@163.com>
2024-02-18 23:10:50 +0800

    d

--- src/composables/booster.ts
+++ src/composables/booster.ts
@@ -16,34 +16,33 @@ export const boosters: Booster[] = [
     {
         cn: '经验加成',
         en: 'affinityBooster',
-        valid: 'https://www.neorae.cn/data/img/booster/Affinity/Gold.png',
-        invalid: 'https://www.neorae.cn/data/img/booster/Affinity/Invalid.png',
+        valid: 'https://warframe.team/cdn/booster/Affinity/Gold.png',
+        invalid: 'https://warframe.team/cdn/booster/Affinity/Invalid.png',
     },
     {
         cn: '现金加成',
         en: 'creditBooster',
-        valid: 'https://www.neorae.cn/data/img/booster/Credit/Gold.png',
-        invalid: 'https://www.neorae.cn/data/img/booster/Credit/Invalid.png',
+        valid: 'https://warframe.team/cdn/booster/Credit/Gold.png',
+        invalid: 'https://warframe.team/cdn/booster/Credit/Invalid.png',
     },
     {
         cn: 'MOD掉落几率加成',
         en: 'modDropRateBooster',
-        valid: 'https://www.neorae.cn/data/img/booster/ModDropRate/Gold.png',
-        invalid:
-            'https://www.neorae.cn/data/img/booster/ModDropRate/Invalid.png',
+        valid: 'https://warframe.team/cdn/booster/ModDropRate/Gold.png',
+        invalid: 'https://warframe.team/cdn/booster/ModDropRate/Invalid.png',
     },
     {
         cn: '资源加成',
         en: 'resourceBooster',
-        valid: 'https://www.neorae.cn/data/img/booster/Resource/Gold.png',
-        invalid: 'https://www.neorae.cn/data/img/booster/Resource/Invalid.png',
+        valid: 'https://warframe.team/cdn/booster/Resource/Gold.png',
+        invalid: 'https://warframe.team/cdn/booster/Resource/Invalid.png',
     },
     {
         cn: '资源掉落几率加成',
         en: 'resourceDropRateBooster',
-        valid: 'https://www.neorae.cn/data/img/booster/ResourceDropRate/Gold.png',
+        valid: 'https://warframe.team/cdn/booster/ResourceDropRate/Gold.png',
         invalid:
-            'https://www.neorae.cn/data/img/booster/ResourceDropRate/Invalid.png',
+            'https://warframe.team/cdn/booster/ResourceDropRate/Invalid.png',
     },
 ]
 
